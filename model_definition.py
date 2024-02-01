ea6eb0ddac99d34c83615a64a4fd3b702df585dd
Ahk <48156018+ahk19@users.noreply.github.com>
2024-02-01 22:48:28 +0300

    Update model_definition.py

--- model_definition.py
+++ model_definition.py
@@ -1,13 +1,5 @@
 import torch
 import torch.nn as nn
-from data_preprocessing import load_and_process_data, vectorize_glove  
-
-# Convert data to torch tensors
-def convert_to_tensors(data):
-    x = torch.tensor([item[1] for item in data], dtype=torch.float32)
-    y = torch.tensor([item[2] for item in data])
-    is_root = torch.tensor([item[3] for item in data])
-    return x, y, is_root
 
 class SimpleRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
